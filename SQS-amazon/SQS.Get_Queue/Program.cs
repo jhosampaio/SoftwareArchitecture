0072b579a7e5e65a13f1772cb14a7c4e96856359
Douglas Prado <douglas.prado@forlogic.net>
2023-06-16 16:23:25 -0300

    message queue initial commit

---
+++ SQS-amazon/SQS.Get_Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Get_Queue
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Fila de mensagem");
+
+            var client = new AmazonSQSClient(RegionEndpoint.USEast2);
+            var request = new ReceiveMessageRequest
+            {
+                QueueUrl = "https://sqs.us-east-2.amazonaws.com/639036197223/architecture"
+            };
+
+            while (true)
+            {
+                var response = await client.ReceiveMessageAsync(request);
+
+                foreach (var mensagem in response.Messages)
+                {
+                    Console.WriteLine(mensagem.Body);
+                    await client.DeleteMessageAsync("https://sqs.us-east-2.amazonaws.com/639036197223/architecture", mensagem.ReceiptHandle);
+                    Console.WriteLine("Mensagem Com id: "+ mensagem.MessageId + " Lida e Apagada");
+                }
+            }
+        }
+    }
+}
